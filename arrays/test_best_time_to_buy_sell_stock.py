56511fcfc9b85445c89b9e23832f0985417d0855
Mohammed Saqib <113831139+saqib777@users.noreply.github.com>
2026-04-04 20:52:53 +0530

    Update test_best_time_to_buy_sell_stock.py

--- arrays/test_best_time_to_buy_sell_stock.py
+++ arrays/test_best_time_to_buy_sell_stock.py
@@ -1,1 +1,27 @@
+import pytest
+from best_time_to_buy_sell_stock import max_profit
 
+
+def test_basic_profit():
+    assert max_profit([7, 1, 5, 3, 6, 4]) == 5
+
+def test_no_profit_descending():
+    assert max_profit([7, 6, 4, 3, 1]) == 0
+
+def test_two_elements_profit():
+    assert max_profit([1, 2]) == 1
+
+def test_two_elements_no_profit():
+    assert max_profit([2, 1]) == 0
+
+def test_single_element():
+    assert max_profit([5]) == 0
+
+def test_empty_array():
+    assert max_profit([]) == 0
+
+def test_all_same_price():
+    assert max_profit([3, 3, 3, 3]) == 0
+
+def test_profit_at_end():
+    assert max_profit([1, 2, 3, 4, 5]) == 4
