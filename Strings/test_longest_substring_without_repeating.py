aec0f69e4f05946f1cefb6aec04ccfc6d52167af
Mohammed Saqib <113831139+saqib777@users.noreply.github.com>
2026-04-04 20:58:22 +0530

    Update test_longest_substring_without_repeating.py

--- Strings/test_longest_substring_without_repeating.py
+++ Strings/test_longest_substring_without_repeating.py
@@ -1,1 +1,28 @@
 
+import pytest
+from longest_substring_without_repeating import length_of_longest_substring
+
+
+def test_basic_case():
+    assert length_of_longest_substring("abcabcbb") == 3
+
+def test_all_same():
+    assert length_of_longest_substring("bbbbb") == 1
+
+def test_mixed():
+    assert length_of_longest_substring("pwwkew") == 3
+
+def test_empty_string():
+    assert length_of_longest_substring("") == 0
+
+def test_single_char():
+    assert length_of_longest_substring("a") == 1
+
+def test_all_unique():
+    assert length_of_longest_substring("abcdef") == 6
+
+def test_spaces():
+    assert length_of_longest_substring("ab c") == 4
+
+def test_numbers_in_string():
+    assert length_of_longest_substring("1234512") == 5
